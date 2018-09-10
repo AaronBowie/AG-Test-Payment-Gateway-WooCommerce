a03fb91e55d82e1268d334e466c6fd96dfcfb984
Aaron <aaron@agdesignstudio.co.uk>
2018-09-10 15:11:23 +0100

    First load out.

---
+++ ag-test.php
@@ -0,0 +1,19 @@
+<?php
+# @Author: Aaron Bowie
+# @Date:   Monday, September 10th 2018, 3:06:30 pm
+# @Email:  support@weareag.co.uk
+# @Filename: ag-test.php
+# @Last modified by:   Aaron Bowie
+# @Last modified time: Monday, September 10th 2018, 3:07:46 pm
+
+
+/*
+Plugin Name: AG Test WooCommerce Gateway
+Plugin URI: https://www.weareag.co.uk/
+Description: Extends WooCommerce By Adding A Test Payment Gateway.
+Version: 0.1
+Author: Aaron Bowie, We are AG
+Author URI: https://www.weareag.co.uk/
+WC requires at least: 3.0.0
+WC tested up to: 3.4.4
+*/
